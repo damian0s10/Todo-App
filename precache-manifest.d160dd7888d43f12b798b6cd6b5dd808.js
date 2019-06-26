551b7710fae4dd2c2b7e6d7a4384477020679569
damian0s10 <damian.wasiluk10@gmail.com>
2019-06-26 22:31:49 +0200

    Updates

---
+++ precache-manifest.d160dd7888d43f12b798b6cd6b5dd808.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7726281346fb58495da3b1b602e8401b",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "ecea921ee9fca2b5c9be",
+    "url": "/Todo-App/static/css/main.fa3b1de0.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/Todo-App/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "ecea921ee9fca2b5c9be",
+    "url": "/Todo-App/static/js/main.3a255335.chunk.js"
+  },
+  {
+    "revision": "2acd21dc4a6582beab29",
+    "url": "/Todo-App/static/js/runtime~main.3fb21290.js"
+  }
+]);
